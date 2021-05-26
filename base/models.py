7caf6d7c0d6e98c380b8ca14f8432d8c22995516
Subham Panda <im.sspanda10@gmail.com>
2021-05-26 21:59:45 +0530

    feat: models for bill vendor expenseid

--- base/models.py
+++ base/models.py
@@ -80,6 +80,7 @@ class Employee(CustomUser):
     objects = EmployeeManager()
     class Meta:
         proxy = True
+    
 class Supervisor(CustomUser):
     default_type = CustomUser.Types.SUPERVISOR
     objects = SupervisorManager()
@@ -98,3 +99,51 @@ class Employee(CustomUser):
 class EmployeeAdditional(models.Model):
     user = OneToOneField(CustomUser,on_delete=models.CASCADE,related_name="emp")
     supervisor = ForeignKey(Supervisor,on_delete=models.CASCADE,related_name="empsupervisor")
+
+class ExpenseID(models.Model):
+    eid = models.CharField(max_length=225)
+    epattern = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.eid
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=225)
+    email = models.EmailField(unique=True)
+    expense_ids = models.ManyToManyField(ExpenseID)
+
+    def __str__(self):
+        return self.name
+
+class BillOther(models.Model):
+    description = models.CharField(max_length=255)
+    is_type_discount = models.BooleanField() #True for discount False for Extra cost
+    amount = models.DecimalField(decimal_places=10,max_digits=10)
+
+class Bill(models.Model):
+    vendor = models.ForeignKey(Vendor,on_delete=models.CASCADE)
+    invoice_num = models.CharField(max_length=100)
+    invoice_date = models.DateField()
+    expense_id = models.CharField(max_length=255)
+    exp_from_date = models.DateField()
+    exp_to_date = models.DateField()
+    quantity = models.IntegerField()
+    rate=models.DecimalField(decimal_places=10,max_digits=10)
+    amount = models.DecimalField(decimal_places=10,max_digits=10)
+    gst = models.DecimalField(decimal_places=10,max_digits=10)
+    other=models.ForeignKey(BillOther, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(decimal_places=10,max_digits=10)
+    due_payment = models.DateField()
+
+    def __str__(self):
+        return self.invoice_num
+
+
+class BillImage(models.Model):
+    image = models.FileField()
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image.url
+
+
