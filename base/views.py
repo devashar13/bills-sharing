f856580af55e69621f7e7e70b5d8bb8e0ff321c5
Dev Ashar <devashar13@gmail.com>
2021-05-26 19:45:01 +0530

    user login backend

--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+def loginView(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print(email,password)
+        user = authenticate(request,email = email,password = password)
+        if user is not None:
+            login(request,user)
+            return render(request,'base/login.html',{"data":"hello"})
+        else:
+            print('nope')
+    else:
+        return render(request,'base/login.html',{})
